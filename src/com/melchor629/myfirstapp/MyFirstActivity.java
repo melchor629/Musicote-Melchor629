d88a0347980bcfa366ef2715c8e4f041679e96ef
Melchor Garau Madrigal <melchor9000@gmail.com>
2012-12-28 19:28:35 +0100

    Adding headers

--- src/com/melchor629/myfirstapp/MyFirstActivity.java
+++ src/com/melchor629/myfirstapp/MyFirstActivity.java
@@ -27,6 +27,25 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.app.ListActivity;
 
+/**
+ * Musicote App
+ * Melchor629 2012
+ *
+ *    Copyright 2012 Melchor629
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+**/
+
 public class MyFirstActivity extends ListActivity {
 
 	public final static String EXTRA_MESSAGE = "com.melchor629.musicote.MESSAGE";
