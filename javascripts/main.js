7daee91572a42548ba1d49b391bbacffb8c73de5
melchor629 <melchor9000@gmail.com>
2012-12-29 21:15:33 +0100

    Si es android, que descargue el APK

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,6 @@
 console.log('This would be the main JS file.');
+var ua = navigator.userAgent.toLowerCase();
+var isAndroid = ua.indexOf("android") > -1;
+if(isAndroid) {
+ window.location.href = 'http://reinoslokos.no-ip.org/com.melchor629.musicote.apk';
+}
