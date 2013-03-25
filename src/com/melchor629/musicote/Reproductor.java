a95255aff31bbca72d2e2cd4effc4ab9c12ed533
Melchor629 <melchor9000@gmail.com>
2013-03-25 19:56:47 +0100

    Mas errores random
    No se porqué pero ahora me sale un "Cannot find symbol" en el
    reproductor con la clase TaskStackBuilder

--- src/com/melchor629/musicote/Reproductor.java
+++ src/com/melchor629/musicote/Reproductor.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import com.melchor629.musicote.scrobbler.Auth;
 import com.melchor629.musicote.scrobbler.Scrobble;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -14,7 +13,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.preference.PreferenceManager;
