4e3364cbc755e7df572785859af0024fdc569c23
Melchor629 <melchor9000@gmail.com>
2013-05-08 21:29:15 +0200

    Moar code

--- src/com/melchor629/musicote/DB.java
+++ src/com/melchor629/musicote/DB.java
@@ -12,6 +12,8 @@ import android.provider.BaseColumns;
  */
 public abstract class DB implements BaseColumns {
 	
+	public static final int    DATABASE_VERSION = 1;
+	public static final String DATABASE_MUSICOTE = "musicote.db";
 	public static final String TABLE_CANCIONES = "canciones";
 	public static final String COLUMN_NAME_ID = "id";
 	public static final String COLUMN_NAME_TITULO = "titulo";
@@ -19,10 +21,15 @@ public abstract class DB implements BaseColumns {
 	public static final String COLUMN_NAME_ALBUM = "album";
 	public static final String COLUMN_NAME_ARCHIVO = "archivo";
 	public static final String COLUMN_NAME_DURACION = "duracion";
+	
+	private final String CREATE_CANCIONES = "CREATE TABLE " + TABLE_CANCIONES + " IF NOT EXISTS (" + COLUMN_NAME_ID + " INTEGER PRIMARY KEY ASC, "
+			+ COLUMN_NAME_TITULO +", " + COLUMN_NAME_ARTISTA + ", " + COLUMN_NAME_ALBUM + ", " + COLUMN_NAME_ARCHIVO + ", "
+			+ COLUMN_NAME_DURACION + ");";
+	private final String DELETE_CANCIONES = "DROP TABLE IF EXISTS " + TABLE_CANCIONES;
 
 	/**
 	 * No constructor for DB
 	 */
 	private DB() { }
-	
+
 }
